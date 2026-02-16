30ad7c8d0425e5a77d2716f239e9025344b8fff7
vinod464 <iqneo7pro143@gmail.com>
2026-02-16 21:06:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -176,9 +176,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://key-gloria-ravikumar-c69e0d15.koyeb.app/".format(FQDN)
+    URL = "https://quixotic-janetta-ravikumar-d47d740f.koyeb.app/".format(FQDN)
 else:
-    URL = "https://key-gloria-ravikumar-c69e0d15.koyeb.app/".format(FQDN)
+    URL = "https://quixotic-janetta-ravikumar-d47d740f.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
