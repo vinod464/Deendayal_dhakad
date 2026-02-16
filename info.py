f5de534a909a171981b69d57c24b1a700bdf068b
vinod464 <iqneo7pro143@gmail.com>
2026-02-16 21:20:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1001
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/SwT0krrj/x.jpg')
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '@Vinuvins')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '@vinod.64@superyes')
 
 # ============================
 # MongoDB Configuration
