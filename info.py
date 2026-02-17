b20a32ee0f8be733c39a66c6337d2c666583b87f
vinod464 <iqneo7pro143@gmail.com>
2026-02-17 20:15:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,7 +96,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 GRP_LNK = environ.get('GRP_LNK', '')
 CHNL_LNK = environ.get('CHNL_LNK', '')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Vinuvins')
-DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/KannadaMagaa')
+DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+TWPh_CxArAQ5YWM1')
 OWNERID = int(os.environ.get('OWNERID', '1329854734'))  # Replace with the actual admin ID
 
 # ============================
